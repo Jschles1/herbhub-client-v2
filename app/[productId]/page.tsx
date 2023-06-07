d37bef56ea28d716ea39de00f44e6edebb619e2c
Jschles1 <johnschlesinger5@gmail.com>
2023-06-07 17:40:51 -0400

    Exceptions, error boundaries and loading state set up

---
+++ app/[productId]/page.tsx
@@ -0,0 +1,19 @@
+import { PageProps } from '@/lib/interfaces';
+import { NotFoundError } from '@/lib/exceptions';
+
+export default async function PDP({ params }: PageProps) {
+  // const productId = params!.productId;
+  // const response = await fetch(
+  //   `https://jsonplaceholder.typicode.com/posts/${productId}`
+  // );
+
+  // if (!response.ok) {
+  //   throw new NotFoundError();
+  // }
+
+  // const data = await response.json();
+
+  // console.log(data);
+  // console.log(params, searchParams);
+  return <div className="w-full h-full flex">PDP</div>;
+}
