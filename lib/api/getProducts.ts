d7dd4aa8dc6fe0ac56cd50926b537b00598f1680
Jschles1 <johnschlesinger5@gmail.com>
2023-06-12 18:50:16 -0400

    getProducts clean up

--- lib/api/getProducts.ts
+++ lib/api/getProducts.ts
@@ -1,21 +1,8 @@
-// export async function getProducts(searchParams?: Record<string, string>) {
-//   let queryFilters = '?' + new URLSearchParams(searchParams).toString();
-//   if (queryFilters === '?') queryFilters = '';
-//   console.log('queryFilters', queryFilters);
-//   const url = `${process.env.API_DOMAIN}/getProducts${queryFilters}`;
-//   const response = await fetch(url);
-//   const data = await response.json();
-//   return data;
-// }
-
-import { QueryFunctionContext } from '@tanstack/react-query';
 import axios from 'axios';
-// import axios from 'axios';
-// import { Product } from './interfaces';
 
 export default async function getProducts(param?: string): Promise<any> {
   try {
-    console.log('1. GET PRODUCTS FETCHER', param);
+    console.log('1. GET PRODUCTS FETCHER', param || '(no param)');
     const queryParams = param && param.length ? `?${param}` : '';
     let domain = process.env.API_DOMAIN + '/';
     if (domain === 'undefined/') {
