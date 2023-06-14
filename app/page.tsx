21fcc41ea91b4b82c5593d6f7b3af2a03174b437
Jschles1 <johnschlesinger5@gmail.com>
2023-06-14 15:15:24 -0400

    Remove getProducts call in SSR

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { PageProps } from '@/lib/interfaces';
 import HomeTemplate from '@/components/templates/home-template';
-import getProducts from '@/lib/api/getProducts';
 
 export default async function Home({ params, searchParams }: PageProps) {
   console.log('HOME SERVER RENDER');
-  // const data = await getProducts();
   const data = { message: 'Hello from the server render', params: '' };
   console.log('DATA', data);
   return <HomeTemplate initialData={data} />;
