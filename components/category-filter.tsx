94fe63c86d217f0bf7c99d0065646dc6c8fc31f0
Jschles1 <johnschlesinger5@gmail.com>
2023-06-14 15:16:27 -0400

    CategoryFilter HTML structure

--- components/category-filter.tsx
+++ components/category-filter.tsx
@@ -1,25 +1,47 @@
 'use client';
 
-import { useQueryParams } from '@/lib/store';
 import * as React from 'react';
+import { Checkbox } from './ui/checkbox';
+import { useQueryParams } from '@/lib/store';
+import { CATEGORY_FILTERS } from '@/lib/constants';
 
 export default function CategoryFilter() {
   const [_, dispatch] = useQueryParams();
 
-  async function handleClick() {
+  async function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+    console.log('CLICKED', event.target);
+
     // Test action
-    await dispatch({
-      type: 'filter',
-      payload: { value: 'str/indica', checked: true },
-    });
+    // await dispatch({
+    //   type: 'filter',
+    //   payload: { value: 'str/indica', checked: true },
+    // });
   }
 
   return (
-    <button
-      className="border border-red-500 w-[300px] p-2"
-      onClick={handleClick}
-    >
-      CategoryFilter
-    </button>
+    <div className="border border-red-500 lg:w-[300px] p-4">
+      {CATEGORY_FILTERS.map((filter) => (
+        <div key={filter.name}>
+          <div className="font-bold">{filter.name}</div>
+          {filter.options.map((option) => (
+            <div key={option.name} className="items-top flex space-x-2">
+              <Checkbox
+                id={option.name}
+                onClick={handleClick}
+                value={option.value}
+              />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor={option.name}
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  {option.name}
+                </label>
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
